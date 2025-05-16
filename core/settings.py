f75e570d6e979253fd6d514888e4e5ad39ed8266
Florin Pinta <xttrust@live.com>
2025-05-16 23:20:17 +0100

    fix: Fixed static files

--- core/settings.py
+++ core/settings.py
@@ -116,16 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # this is the key line
+    BASE_DIR / 'static',
 ]
 
-# Optional: Improve static files performance
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
